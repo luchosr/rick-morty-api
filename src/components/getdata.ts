f6fbb17892bc516c6d333517ab0fd220dcce153d
luciano.ramello <luciano.ramello@vass.es>
2021-04-08 18:00:00 +0200

    add get method

--- src/components/getdata.ts
+++ src/components/getdata.ts
@@ -13,6 +13,18 @@ export class GetData extends LitElement{
         super()
         console.log('GetData working!!');
     }
+
+    _sendData(data){
+        this.dispatchEvent(new CustomEvent('ApiData',{
+            detail:{data}, bubbles:true, composed:true
+        }))
+    }
+    getData(){
+        fetch(this.url,{method: this.method})
+        .then(response =>{
+            // quedaste en 8:40 https://www.youtube.com/watch?v=TbG7U16n4fY
+        })
+    }
 }
 
 customElements.define('get-data',GetData )
